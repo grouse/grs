37a1fdb36cdc0419e72667d178da7038e9ce6d1b
Jesper Stefansson <jesper.stefansson@gmail.com>
2024-05-30 14:10:03 +0200

    mat3_tranpose rename and out of bounds access fix

--- maths.cpp
+++ maths.cpp
@@ -478,28 +478,20 @@ Matrix3 transform(Matrix3 projection, Vector2 position, f32 uni_scale) EXPORT
     return projection*mview*mscale;
 }
 
-Matrix3 transpose(Matrix3 m) EXPORT
+Matrix3 mat3_transpose(Matrix3 m) EXPORT
 {
     Matrix3 r;
     r[0][0] = m[0][0];
     r[0][1] = m[1][0];
     r[0][2] = m[2][0];
-    r[0][3] = m[3][0];
 
     r[1][0] = m[0][1];
     r[1][1] = m[1][1];
     r[1][2] = m[2][1];
-    r[1][3] = m[3][1];
 
     r[2][0] = m[0][2];
     r[2][1] = m[1][2];
     r[2][2] = m[2][2];
-    r[2][3] = m[3][2];
-
-    r[3][0] = m[0][3];
-    r[3][1] = m[1][3];
-    r[3][2] = m[2][3];
-    r[3][3] = m[3][3];
     return r;
 }
 
