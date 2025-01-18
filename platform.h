36832bed0f932bab22f775828efd4ada63a9bf72
Jesper Stefansson <jesper.stefansson@gmail.com>
2025-01-18 11:18:00 +0100

    error message in static_asserts for the primitive integer/float sizeof checks

--- platform.h
+++ platform.h
@@ -88,18 +88,18 @@ constexpr f32 f32_PI = (f32)f64_PI;
 #define u16_MAX (u16)0xFFFF
 
 
-static_assert(sizeof(u64) == 8);
-static_assert(sizeof(i64) == 8);
-static_assert(sizeof(f64) == 8);
+static_assert(sizeof(u64) == 8, "u64 sizeof mismatch");
+static_assert(sizeof(i64) == 8, "i64 sizeof mismatch");
+static_assert(sizeof(f64) == 8, "f64 sizeof mismatch");
 
-static_assert(sizeof(u32) == 4);
-static_assert(sizeof(i32) == 4);
-static_assert(sizeof(f32) == 4);
+static_assert(sizeof(u32) == 4, "u32 sizeof mismatch");
+static_assert(sizeof(i32) == 4, "i32 sizeof mismatch");
+static_assert(sizeof(f32) == 4, "f32 sizeof mismatch");
 
-static_assert(sizeof(u16) == 2);
-static_assert(sizeof(i16) == 2);
+static_assert(sizeof(u16) == 2, "u16 sizeof mismatch");
+static_assert(sizeof(i16) == 2, "i16 sizeof mismatch");
 
-static_assert(sizeof(u8) == 1);
-static_assert(sizeof(i8) == 1);
+static_assert(sizeof(u8) == 1, "u8 sizeof mismatch");
+static_assert(sizeof(i8) == 1, "i8 sizeof mismatch");
 
 #endif // PLATFORM_H
